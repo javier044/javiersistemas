24ea2aad782f5c9ef1622cc993aad7bdb4b24276
javier044 <anetyjavi@hotmail.com>
2018-12-02 00:04:50 -0600

    cambio de imagenes

--- login/index.php
+++ login/index.php
@@ -24,7 +24,7 @@ if(isset($_SESSION['session']))
 		<div id="contenedor">
 	
 			<div id="cabecera" >
-				<img src="./css/images/infinio.jpg">
+				<img src="./css/photos/infinio.jpg">
 			</div>
 	
 			<div id="cuerpo">
