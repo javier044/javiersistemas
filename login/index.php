0f2fc6b3fe0cd46d2bb834c75a96c68b57f585df
javier044 <anetyjavi@hotmail.com>
2018-12-04 06:56:40 -0600

    cambio de name

--- login/index.php
+++ login/index.php
@@ -38,7 +38,7 @@ if(isset($_SESSION['sesion']))
 					<p><label>Contraseña:</label></p>
 						<input name="flor" type="contraseña" id="contraseña" placeholder="Ingresa contraseña" required=""></p>
 					
-					<p id="bot"><input type="submit" id="submit" name="usuario" value="registro" class="botón"></p>
+					<p id="bot"><input type="submit" id="submit" nombre="usuario" value="registro" class="botón"></p>
 				</form>
 			</div><!--fin cuerpo-->
 	
